b1cefd5151cc8a8b0188b4c2f58c2759f7a35149
taramaranda <68873792+taramaranda@users.noreply.github.com>
2021-02-17 11:53:05 -0600

    Update

---
+++ js/index.js
@@ -0,0 +1,22 @@
+console.log("Your index.js file is loaded correctly!");
+
+<body>
+  body {font-family: Arial, Helvetica, sans-serif;}
+  
+  $("#myID").on("click", function()
+    $(".fadeMe").fadeToggle("slow); {
+  <body>
+  
+  <h1>JavaScript console.log() Method</h1>
+  
+  <p>Press F12 on your keyboard to view the message in the console view.</p>
+  
+  <p>This example demonstrates how an object is displayed in the console view.</p>
+  
+  <script>
+  var myObj = { firsname : "Tara", lastname : "Maranda" };
+  console.log(myObj);
+  </script>
+  
+  </body>
+});
